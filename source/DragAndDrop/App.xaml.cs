67b08eee6f8453a1dba645b94ba34d4d372588b8
OiChan <makoto.uwaya@gmail.com>
2019-02-10 16:16:43 +0900

    Convert Prism.Unity Implements

--- source/DragAndDrop/App.xaml.cs
+++ source/DragAndDrop/App.xaml.cs
@@ -1,9 +1,29 @@
-﻿namespace DragAndDrop
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace DragAndDrop
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class Application
+    public partial class Application : PrismApplication
     {
+        /// <summary>
+        /// Startup window
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateShell()
+        {
+            return this.Container.Resolve<MainWindow>();
+        }
+
+        /// <summary>
+        /// Resist type to container
+        /// </summary>
+        /// <param name="containerRegistry"></param>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
     }
 }
